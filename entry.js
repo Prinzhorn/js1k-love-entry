dee215677a7390aa0dcc2a07b8de983f5d932ab3
Alexander Prinzhorn <alex@nder-prinzhorn.de>
2012-02-05 11:12:33 +0100

    golfed away 68 bytes

--- entry.js
+++ entry.js
@@ -9,8 +9,8 @@ var
 	j = r/2,
 	k = s/2,
 
-	//The array containing the hearts. First heart is centered and red
-	h = [[1, j, k, 'red']],
+	//The array containing the hearts
+	h = [],
 
 	//For computing sine. Will increment over time
 	t = 0,
@@ -18,32 +18,38 @@ var
 	P = Math.PI,
 	S = Math.sin;
 
-setInterval(function(i) {
-	t += .005;
+//Now we can use b and c as usual variables, because we don't need body or canvas
 
-	//Remove the FIRST element if the SECOND is big enough
-	//h[1]&&h[1][0]>j&&h.splice(0,1);
-	h[1]&&h[1][0]>j&&h.shift();
+setInterval(function(i, g) {
+	t += .004;
 
 	for(;h[i];i++)with(a) {
 		b = h[i];
-		b[0] *= 1.1;
+		g = b[0] *= 1.1;
 
 		fillStyle = b[3];
 
 		lineWidth = .2;
-		strokeStyle = 'black';
 
 		beginPath();
-		arc(b[1] - b[0], c = b[2] - b[0], b[0], P*.8, P*2);
-		arc(b[1] + b[0], c, b[0], P, P*2.2);
-		lineTo(b[1], b[2] + b[0]*2);
+		arc(b[1] - g, c = b[2] - g, g, P*.8, P*2);
+		arc(b[1] + g, c, g, P, P*2.2);
+		lineTo(b[1], b[2] + g*2);
 		closePath();
 
 		fill();
 		stroke();
 	}
 
-	//Append a new element if the last element is big enough
-	if(h[0]&&h[i-1][0] > 1.15) h.push([1, (c=S(t)/2)*j+j, c*k+k, 'rgb(' + ((Math.random()*55+200) | 0) + ',0,0)']);
+	//var x = x/s/.02 - (h.x*50/s);
+	//var y = -y/s/.02 + (h.y*50.5/s);
+
+	//(x*x + 2 * (b = (y - .9 * Math.sqrt( x<0?-x:x ))) * b)
+
+
+	//Append a new element if the array is empty OR if the last element is big enough
+	(!i || g > 1.15) && h.push([1, (c=S(t)/2)*j+j, c*k+k, 'rgb(' + ((Math.random()*55+200) | 0) + ',0,0)']);
+
+	//Remove the first element if big enough
+	h[0][0]>j&&h.shift();
 }, 33, 0);
