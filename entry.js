c59bcab0d41a62cf93e280f7982e6d9d9cb7d236
Alexander Prinzhorn <alex@nder-prinzhorn.de>
2012-02-07 18:37:45 +0100

    more golfed, less readable

--- entry.js
+++ entry.js
@@ -7,7 +7,12 @@
 
 /*
  * Props for the idea on how to draw a heart go to: http://www.mathematische-basteleien.de/heart.htm
- * I also had a working version using single pixels and a function, but drawing arcs and lines is WAY faster.
+ * I also had a working version using single pixels and a function (see below), but drawing arcs and lines is WAY faster.
+ *
+ * Original heart function
+ * var x = x/s/.02 - (h.x*50/s);
+ * var y = -y/s/.02 + (h.y*50.5/s);
+ * (x*x + 2 * (b = (y - .9 * Math.sqrt( x<0?-x:x ))) * b) && (...draw red pixel...)
  *
  * Each heart is stored inside an array: [size, x, y, color]
  */
@@ -27,32 +32,21 @@ var
 
 	//For computing sine. Will increment over time.
 	//And we set the bodys margin to 0 (b is now the body's style prop)
-	t = b.margin= 0,
+	t = b.margin = 0,
 
 	P = Math.PI,
 	C = Math.cos;
 
-//Now we can use b and c as usual variables, because we don't need body or canvas
+
+//Now we can use b and c as usual variables, because we won't need body or canvas
+
 
 setInterval(function(i, g) {
-	//Time is moving forward ;-)
-	t += .004;
-
 	//Now scope everything to the canvas context, because we are doing a shitload of method calls
-	with(a) {
-		save();
-
-		//Rotate around center (lean into the turn)
-		//translate(j, k);
-		//rotate(C(t+P/2)/4);
-		//translate(-j, -k);
-
-		//Keep it centered
-		//translate(-C(t)*j/2, -C(t)*k/2);
-		translate(-C(t)*j/2, 0);
-
-		//Iterate over all hearts (h[i] will be undefined at some time)
-		for(;h[i];i++) {
+	with(a)
+		//Iterate over all hearts (h[i] will be undefined at some time, which will then trigger restore())
+		//The translation will keep it centered
+		for(save() || translate(-C(t+=.004)*j/2, 0); h[i] || restore(); i++) {
 			//Keep track of the current heart. Saves bytes instead of writing h[i] all the time
 			b = h[i];
 
@@ -60,54 +54,47 @@ setInterval(function(i, g) {
 			//This value will also come in handy after the loop is finished, because we need the size of the last heart for removing it
 			g = b[0] *= 1.1;
 
-			//TODO instead of hiding smal hearts, they should actually be visible depending on the curve
-			//if(g < 3) {
-				//continue;
-			//}
+			/*
+			 * blame @jedschmidt for this looking so fucked up
+			 * we are basically saving semicolons by nesting function calls
+			 * start reading from inner most expression (which is still not exactly the order it executes)!
+			 */
 
-			//The fourth position of a heart array is the color
-			fillStyle = b[3];
+			//At the end, fill the heart and add a stroke
+			stroke(
+				fill(
+					//The left part (close the heart). Only needed because we are drawing a stroke! Wouldn't for just fills.
+					closePath(
+						//The right lower part of the "peak"
+						lineTo(
+							//Parameters for the line segment (x, y + size*2)
+							b[1], b[2] + g*2,
 
-			//Some decent black (=== default strokeStyle) line
-			lineWidth = .4;
+							//Right arc/curve of the heart
+							arc(
+								//Parameters for drawing the right (x + size, y - size, some radian, some radian)
+								b[1] + g, c = b[2] - g, g, P, P*2.2,
 
-			beginPath();
+								//Left arc/curve of the heart
+								arc(
+									//Parameters for drawing the left arc (x - size, y - size, some radian, some radian)
+									b[1] - g, c, g, P*.8, P*2,
 
-			//Left circle part
-			arc(b[1] - g, c = b[2] - g, g, P*.8, P*2);
-
-			//Right circle part
-			arc(b[1] + g, c, g, P, P*2.2);
-
-			//The right lower part of the "peak"
-			lineTo(b[1], b[2] + g*2);
-
-			//The left part. Only needed because we are drawing a stroke! Not for fills.
-			closePath();
-
-			globalCompositeOperation = 'destination-out';
-
-			fill();
-
-			globalCompositeOperation = 'source-over';
-
-			fill();
-
-			stroke();
+									//Start a new path for our heart
+									beginPath(
+										//Some decent black (=== default strokeStyle) thin line
+										fillStyle = b[3], lineWidth = .2
+									)
+								)
+							)
+						)
+					)
+				)
+			)
 		}
-
-		restore();
-	}
-
-
-	//var x = x/s/.02 - (h.x*50/s);
-	//var y = -y/s/.02 + (h.y*50.5/s);
-
-	//(x*x + 2 * (b = (y - .9 * Math.sqrt( x<0?-x:x ))) * b)
 
 
 	//Append a new element if the array is empty OR if the last element is big enough
-	//(!i || g > 1.15) && (h[i]=[1, (c=C(t)/2)*j+j, c*k+k, 'rgb(' + ((Math.random()*55+200) | 0) + ',0,0)']);
 	(!i || g > 1.2) && (h[i]=[1, (c=C(t)/2)*j+j, k, 'rgb(' + ((Math.random()*55+200) | 0) + ',0,0)']);
 
 	//Remove the first element if big enough
