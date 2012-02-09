69a12306e5d52e458c791c06e63db11ba60b8e8a
Alexander Prinzhorn <alex@nder-prinzhorn.de>
2012-02-09 19:58:47 +0100

    added clipping and player now stays centered

--- entry.js
+++ entry.js
@@ -1,7 +1,14 @@
 /*
  * TODO:
+ * -!!!!!!!!!!!call fill and stroke only once! use moveTo in loop!!!!!
+ * 		Nope, beause different colors
  * -clipping (more curvy turns)
- * -remove sine/cosine in favour of heart shaped turns
+ * -remove sine/cosine in favour of heart shaped turns (maybe not, because steering on one axis is enough). BUT we could spice it up by not using the standard sine-
+ * -add an arrow
+ * -make it controllable
+ * -collision detection
+ * -highscore (t (number of ticks))
+ * -tweet highscore
  */
 
 /*
@@ -13,7 +20,7 @@
  * var y = -y/s/.02 + (h.y*50.5/s);
  * (x*x + 2 * (b = (y - .9 * Math.sqrt( x<0?-x:x ))) * b) && (...draw red pixel...)
  *
- * Each heart is stored inside an array: [size, x, y, color]
+ * Each heart is stored inside an array: [size, x, y, color, time added]
  */
 
 //"with" isn't worth it for two properties.
@@ -41,11 +48,12 @@ var
 
 
 setInterval(function(i, g) {
+t++;
 	//Now scope everything to the canvas context, because we are doing a shitload of method calls
 	with(a)
 		//Iterate over all hearts (h[i] will be undefined at some time, which will then trigger restore())
 		//The translation will keep it centered
-		for(save() || translate(-C(t++/250)*j/2, 0); b = h[i] || restore(); i++) {
+		for(save() || translate(-C((h[8]&&h[8][4]||t)/100)*j/2, 0); b = h[i] || restore() || restore(); i++) {
 
 			//Make the heart bigger and keep track of the new size, because we need this value often
 			//This value will also come in handy after the loop is finished, because we need the size of the last heart for removing it
@@ -57,9 +65,9 @@ setInterval(function(i, g) {
 			 * start reading from inner most expression (which is still not exactly the order it executes)!
 			 */
 
-			//At the end, fill the heart and add a stroke
-			stroke(
-				fill(
+			//At the end, fill the heart and add a stroke around
+			//stroke(
+				//fill(
 					//The left part (close the heart). Only needed because we are drawing a stroke! Wouldn't for just fills.
 					closePath(
 						//The right lower part of the "peak"
@@ -79,19 +87,40 @@ setInterval(function(i, g) {
 
 									//Start a new path for our heart
 									beginPath(
-										//Some decent black (=== default strokeStyle) thin line
-										fillStyle = b[3], lineWidth = .2
+										//Some decent black thin line
+										fillStyle = b[3], strokeStyle = '#000', lineWidth = .2
 									)
 								)
 							)
 						)
-					)
-				)
-			)
+					);
+
+			fill();
+
+			i%20||clip();
+			//stroke();
+
+			//clip();
 		}
 
+	//a.fillStyle = 'rgba(255, 255, 200, 0.5)';
+	//a.arc(j, k, (c=C(t/15-P))*c*c*c*20, -P, P);
+	//a.fill();
+
 	//Append a new element if the array is empty OR if the last element is big enough
-	(!i || g > 1.2) && (h[i]=[1, (c=C(t/250)/2)*j+j, k, 'rgb(' + ((200*(c=C(t/15))*c*c*c+40)|0) + ',0,0)']);
+	(!i || g > 1.2) && (h[i]=[1, (c=C(t/100)/2)*j+j, k, 'rgb(' + (c=(200*(c=C(t/15))*c*c*c+40)|0) + ',0,0)', t]);
+
+	/*
+	a.beginPath();
+	a.moveTo(j, k+9);
+	a.lineTo(j, k+99);
+	a.lineWidth = 5;
+	a.strokeStyle = 'rgb(0, 0, ' + c + ')';
+	a.stroke();
+	*/
+
+	a.fillStyle = 'rgb(0, 0, ' + c + ')';
+	a.fillRect(j-9, k+20, 18, 99);
 
 	//Remove the first element if big enough
 	h[0][0]>j&&h.shift();
